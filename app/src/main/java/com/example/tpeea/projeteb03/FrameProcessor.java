2952525c240d87394faadfa7f413516c021c4cb7
Sawnd <sawnd.the.one@gmail.com>
2018-05-05 17:49:02 +0200

    debut FrameProcessor

---
+++ app/src/main/java/com/example/tpeea/projeteb03/FrameProcessor.java
@@ -0,0 +1,13 @@
+package com.example.tpeea.projeteb03;
+
+/**
+ * Created by Steph on 05/05/2018.
+ */
+
+public class FrameProcessor {
+// On enverra un tableau de bytes brut contenant la commande et les argument, et il faudra renvoyer une trame
+    public byte[] toFrame(byte[] commandes){
+     byte[] frame = null;
+     return frame;
+    }
+}
