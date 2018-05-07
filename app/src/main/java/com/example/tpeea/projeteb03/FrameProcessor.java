18f60d16d479deda84fb0fd39eeab8975681ae07
Sawnd <sawnd.the.one@gmail.com>
2018-05-07 08:16:41 +0200

    essai FrameProcessor

--- app/src/main/java/com/example/tpeea/projeteb03/FrameProcessor.java
+++ app/src/main/java/com/example/tpeea/projeteb03/FrameProcessor.java
@@ -7,7 +7,14 @@ package com.example.tpeea.projeteb03;
 public class FrameProcessor {
 // On enverra un tableau de bytes brut contenant la commande et les argument, et il faudra renvoyer une trame
     public byte[] toFrame(byte[] commandes){
-     byte[] frame = null;
+        byte  identifiant = commandes[0];
+        byte arguments[] =  null;
+        int i = 1;
+        // On lit le tableau à partir de la deuxième entrée jusqu'à trovuer le caractère d'échappement,. On essaye d'obtenir tus les arguments
+        while (commandes[i] !=  0x06 && commandes[i] < commandes.length) {
+
+        }
+        byte[] frame = null;
      return frame;
     }
 }
