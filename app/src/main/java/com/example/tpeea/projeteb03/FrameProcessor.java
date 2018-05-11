3acfc4a2b391f3cfd2200cda7bd3ef440ae79b0d
Sawnd <sawnd.the.one@gmail.com>
2018-05-11 13:58:05 +0200

    implémentation du Frame Processor

--- app/src/main/java/com/example/tpeea/projeteb03/FrameProcessor.java
+++ app/src/main/java/com/example/tpeea/projeteb03/FrameProcessor.java
@@ -10,21 +10,39 @@ public class FrameProcessor {
         byte header = 0x05;
         byte tail = 0x04;
         byte[] length = {0x00, (byte) payload.length};
-        byte ctrl;
+        int sumPayload= toSumTab(payload);
+        int sum = header + length[1] + sumPayload;
+        byte ctrl = new Byte( toComplement(Integer.toHexString(sum)));
 
         byte[] frame = null;
         return frame;
     }
 
-    // Permet d'obtenir le complément à deux d'un hexa
-    int toComplement(String hex) {
+    // Permet d'obtenir le complément à deux d'un hexa, retourne un hexa
+    String toComplement(String hex) {
         int i = Integer.parseInt(hex, 16);
         String bin = Integer.toBinaryString(i);
         // Complement
         String input = bin.replaceAll("0", "a").replaceAll("1", "0").replaceAll("a", "1");
         int number = Integer.parseInt(bin, 2) + 1;
-        return number;
+        return Integer.toHexString(number);
 
 
     }
+    int toComplement2(String hex){
+        int i = Integer.parseInt(hex, 16);
+        i = i%256;
+
+        return 256 - i;
+    }
+
+    int toSumTab(byte[] tab){
+        int total = 0 ;
+        for (int val : tab ) {
+        total += (int) val;
+        }
+        return total;
+    }
+
+
 }
