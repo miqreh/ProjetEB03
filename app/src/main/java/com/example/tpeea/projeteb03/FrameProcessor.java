cc180757fffa34a91bc3ced44864b31e819618ec
Sawnd <sawnd.the.one@gmail.com>
2018-05-11 11:52:15 +0200

    essai FrameProcessor

--- app/src/main/java/com/example/tpeea/projeteb03/FrameProcessor.java
+++ app/src/main/java/com/example/tpeea/projeteb03/FrameProcessor.java
@@ -5,16 +5,26 @@ package com.example.tpeea.projeteb03;
  */
 
 public class FrameProcessor {
-// On enverra un tableau de bytes brut contenant la commande et les argument, et il faudra renvoyer une trame
-    public byte[] toFrame(byte[] commandes){
-        byte  identifiant = commandes[0];
-        byte arguments[] =  null;
-        int i = 1;
-        // On lit le tableau à partir de la deuxième entrée jusqu'à trovuer le caractère d'échappement,. On essaye d'obtenir tus les arguments
-        while (commandes[i] !=  0x06 && commandes[i] < commandes.length) {
+    // On enverra un tableau de bytes brut contenant la commande et les argument (payload), et il faudra renvoyer une trame
+    public byte[] toFrame(byte[] payload) {
+        byte header = 0x05;
+        byte tail = 0x04;
+        byte[] length = {0x00, (byte) payload.length};
+        byte ctrl;
 
-        }
         byte[] frame = null;
-     return frame;
+        return frame;
+    }
+
+    // Permet d'obtenir le complément à deux d'un hexa
+    int toComplement(String hex) {
+        int i = Integer.parseInt(hex, 16);
+        String bin = Integer.toBinaryString(i);
+        // Complement
+        String input = bin.replaceAll("0", "a").replaceAll("1", "0").replaceAll("a", "1");
+        int number = Integer.parseInt(bin, 2) + 1;
+        return number;
+
+
     }
 }
