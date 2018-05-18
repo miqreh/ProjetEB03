88d3d03729249a8db8d44438de4e2ffc84d0c65a
Sawnd <sawnd.the.one@gmail.com>
2018-05-18 11:43:19 +0200

    Comment on FrameProcessor

--- app/src/main/java/com/example/tpeea/projeteb03/FrameProcessor.java
+++ app/src/main/java/com/example/tpeea/projeteb03/FrameProcessor.java
@@ -5,37 +5,34 @@ package com.example.tpeea.projeteb03;
  */
 
 public class FrameProcessor {
-    // On enverra un tableau de bytes brut contenant la commande et les argument (payload), et il faudra renvoyer une trame
+    // On reçoit en paramètres un tableau de byte contenan la commande et ses arguments
     public byte[] toFrame(byte[] commande) {
+        // On construit la trame
         byte header = 0x05;
         byte tail = 0x04;
+        // length indique la taille de la commande
         byte[] length = {0x00, (byte) commande.length};
         byte[] payload = toEchap(commande);
-        int sumPayload= toSumTab(commande);
-        int sum =length[1] + sumPayload;
+        int sum =length[1] +toSumTab(commande);
         byte ctrl = (byte)Integer.parseInt(toComplement2(Integer.toHexString(sum)),16);
-
-
         byte[] frame = new byte[3 + length.length + payload.length];
-        int i=0;
-        frame[i] = header;
-        i++;
-        frame[i]=length[0];
-        i++;
-        frame[i]=length[1];
-        i++;
+
+        // On assemble tous les éléments de la frame pour la construire
+        frame[0] = header;
+        frame[1]=length[0];
+        frame[2]=length[1];
+        int i=3;
         for(byte b:payload){
             frame[i]=b;
             i++;
         }
         frame[i]=ctrl;
-        i++;
-        frame[i]=tail;
+        frame[i+1]=tail;
 
         return frame;
     }
 
-    // Permet d'obtenir le complément à deux d'un hexa, retourne un hexa
+    /* Test complément à deux...
     String toComplement(String hex) {
         int i = Integer.parseInt(hex, 16);
         String bin = Integer.toBinaryString(i);
@@ -43,9 +40,8 @@ public class FrameProcessor {
         String input = bin.replaceAll("0", "a").replaceAll("1", "0").replaceAll("a", "1");
         int number = Integer.parseInt(bin, 2) + 1;
         return Integer.toHexString(number);
-
-
-    }
+    }*/
+    // Renvoie le complement à deux d'un hexadecimal
     String toComplement2(String hex){
         int i = Integer.parseInt(hex, 16);
         i = i%256;
@@ -54,11 +50,11 @@ public class FrameProcessor {
         return  Integer.toHexString(result);
     }
 
-    String toComplement3(String hex){
+/*    String toComplement3(String hex){
         int i = Integer.parseInt(hex, 16);
         return Integer.toHexString(255 - (i / 256) - i / 256 * 256) + 1;
-    }
-
+    }*/
+// Permet de sommer les octets contenu dans un tableau de byte
     int toSumTab(byte[] tab){
         int total = 0 ;
         for (int val : tab ) {
@@ -66,7 +62,7 @@ public class FrameProcessor {
         }
         return total;
     }
-
+// permet d'inserer un caractère d'ecchappemet devant les bytes réservés , ici 0x04,0x05,0x06. Rajoute 0x06 au carcatère suivant et renvoie le tableau de byte échappé
     byte[] toEchap(byte[] b){
         int i=0;
         for (byte bi: b) {
