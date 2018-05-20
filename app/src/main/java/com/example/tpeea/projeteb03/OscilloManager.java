3a2cf00b7529ff80b400ecad36253e4c6fae6b39
Sawnd <sawnd.the.one@gmail.com>
2018-05-20 13:27:52 +0200

    implementation de toutes les commandes

--- app/src/main/java/com/example/tpeea/projeteb03/OscilloManager.java
+++ app/src/main/java/com/example/tpeea/projeteb03/OscilloManager.java
@@ -33,14 +33,55 @@ public class OscilloManager implements Transceiver.TransceiverDataListener, Tran
     public byte[] setVerticalOffset(int channel,int value){
         // Value est sur deux octets doc doit être compris entre
         byte[] commande = {3,0,0};
-        int minBit,maxBit;
+
         if(channel==1){
             commande[1]=0x01;
         }
         String hex = hexString(value);
-
+        byte[] valueBytes = getLsbMsb(hex);
+        commande[1]=valueBytes[1];
+        commande[2]=valueBytes[0];
        return commande;
     }
+
+
+    public byte[] setHorizontalScale(int index){
+        byte[] commande = {7,0};
+        if(index<=16 && index >=0){
+            commande[1]=(byte)index;
+        }
+
+        return commande;
+    }
+    public byte[] setChannel(int channel ,boolean state){
+        byte[] commande = {0x0B,0,0};
+        if(channel==1){
+            commande[1]=0x01;
+        }
+        if(state  == true){
+            commande[2]=0x01;
+        }
+        return commande;
+    }
+
+    public byte[] setCalibrationDutyCycle(int dutyCycle){
+        byte[] commande = {0x0A,0};
+        if(dutyCycle>=0 && dutyCycle<=100){
+            commande[1] = (byte)dutyCycle;
+        }
+        return commande;
+    }
+
+    public byte[] setTriggerChannel(int channel){
+        byte[] commande = {0x08,(byte)channel};
+        return commande;
+    }
+
+    public byte[] setTrigggerLevel(int type){
+        byte[] commande = {0x0C,(byte)type};
+        return commande;
+    }
+
     public String hexString(int i){
         String hex = Integer.toHexString(i);
         StringBuilder result = new StringBuilder("0000");
@@ -55,7 +96,7 @@ public class OscilloManager implements Transceiver.TransceiverDataListener, Tran
         return result.toString();
     }
 
-    public byte[] getMsbLsb(String hex){
+    public byte[] getLsbMsb(String hex){
         byte[] result = {0,0};
         if(hex.length()<=4){
             result[0]=(byte)(Integer.parseInt(Character.toString(hex.charAt(0)),16)+Integer.parseInt(Character.toString(hex.charAt(1)),16)*16);
@@ -79,7 +120,7 @@ public class OscilloManager implements Transceiver.TransceiverDataListener, Tran
         OscilloManager om = OscilloManager.getOscilloManager();
         String hex =om.hexString(a);
         System.out.println(hex);
-        byte[] result =om.getMsbLsb(hex);
+        byte[] result =om.getLsbMsb(hex);
         System.out.print(result);
     }
 }
